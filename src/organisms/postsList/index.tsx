e690f8bd941e297a122c49a8311734e546476af0
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-03-29 16:39:14 +0900

    :sparkles: (blog) add posts list

---
+++ src/organisms/postsList/index.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+import { PostsListQuery } from "../../../types/graphql-types"
+import PostsListRow from "../postsListRow"
+
+interface Props {
+  data: PostsListQuery
+}
+
+export default function PostsList(props: Props): JSX.Element {
+  const { allMarkdownRemark } = props.data
+  return (
+    <div>
+      <p>計 {allMarkdownRemark.totalCount}記事</p>
+      <h4>
+        {allMarkdownRemark.edges.map(({ node }) => {
+          if (node.frontmatter && node.frontmatter.slug) {
+            return <PostsListRow node={node} />
+          }
+        })}
+      </h4>
+    </div>
+  )
+}
---
+++ src/organisms/postsListRow/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "gatsby"
+import React from "react"
+import { MarkdownRemarkFrontmatter } from "../../../types/graphql-types"
+
+interface Props {
+  node: {
+    id: string
+    frontmatter?: MarkdownRemarkFrontmatter | null
+  }
+}
+
+export default function PostsListRow(props: Props): JSX.Element {
+  const { node } = props
+  return (
+    <div key={node.id}>
+      {node.frontmatter && node.frontmatter.slug && (
+        <div>
+          <Link to={node.frontmatter.slug}>
+            <p>{node.frontmatter.title}</p>
+          </Link>
+          <p>{node.frontmatter.date}</p>
+        </div>
+      )}
+    </div>
+  )
+}
