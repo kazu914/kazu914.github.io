afeae97fd240bb14778b0355417c8b460e2ba6ee
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-05-02 20:18:04 +0900

    :sparkles: (about ) add the card to about index page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,17 @@
+import { Typography } from "@material-ui/core"
 import React from "react"
 import Layout from "../../components/layout"
+import { AboutInfoCard } from "../../organisms"
 
 export default function About(): JSX.Element {
   return (
     <Layout>
-      <h1>自己紹介作る</h1>
-      <p>ここに自己紹介のページを作る</p>
+      <div>
+        <Typography variant="h3" align="center" style={{ margin: 40 }}>
+          About me
+        </Typography>
+      </div>
+      <AboutInfoCard />
     </Layout>
   )
 }
