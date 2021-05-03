4127afca58986354bdd4c8b8e1e1c878c3223364
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-05-03 11:31:07 +0900

    :sparkles: (about) add link to each education institute

--- src/organisms/educationInfoCard/index.tsx
+++ src/organisms/educationInfoCard/index.tsx
@@ -9,14 +9,17 @@ export default function EducationInfoCard(): JSX.Element {
         <InfoCardContent
           title="2012-4 〜 2015-3"
           content="Seiseiko High School, Kumamoto"
+          url="http://seiseiko.ed.jp/"
         />
         <InfoCardContent
           title="2015-4 〜 2019-3"
           content="School of Electrical and Electronic Engineering, Osaka Prefecture University"
+          url="https://www.osakafu-u.ac.jp/academics/college/ce/seee/"
         />
         <InfoCardContent
           title="2019-4 〜 2021-3"
           content="Department of Computer Science and Intelligent Systems, Graduate School of Engineering, Osaka Prefecture University"
+          url="http://www.cs.osakafu-u.ac.jp/grad"
         />
       </Grid>
     </Card>
